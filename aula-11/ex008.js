754f09575743cb7289e2aa5756a2ac6bbf6b6bd6
massarutti <sousa.massarutti@icloud.com>
2022-10-10 20:39:17 -0300

    Commit Aula-12 Guanabara

---
+++ aula-11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50;
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+
+if (vel > 60) {
+  console.log(`Voce ultrapassou a velocidade permitida, MULTADO!`);
+}
+
+console.log(`Dirija sempre com segurança`);
